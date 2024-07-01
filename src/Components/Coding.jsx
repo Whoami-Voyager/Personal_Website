a64f7b9696afb4eaac4d12798be8ab7219989bb2
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-07-01 17:01:22 -0600

    updated coding page

--- src/Components/Coding.jsx
+++ src/Components/Coding.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import ProjectCard from "./ProjectCard";
 
 function Coding() {
     return (
@@ -8,10 +7,20 @@ function Coding() {
                 <Link to='/' className="hover:underline">⬅ Home</Link>
             </div>
             <div className="min-h-screen flex flex-col justify-center items-center mx-4 text-center">
-                <div className="flex flex-row items-center space-x-4">
-                    <img src="https://cdn-icons-png.flaticon.com/128/3967/3967195.png" alt="Under Construction Icon"/>
-                    <h1 className="font-ttNorms text-3xl">This part is under construction. Come back later.</h1>
-                    <img src="https://cdn-icons-png.flaticon.com/128/3967/3967195.png" alt="Under Construction Icon"/>
+                <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4">
+                    <img
+                        src="https://cdn-icons-png.flaticon.com/128/3967/3967195.png"
+                        alt="Under Construction Icon"
+                        className="w-24 h-24 md:w-32 md:h-32"
+                    />
+                    <h1 className="font-ttNorms text-2xl md:text-3xl lg:text-4xl">
+                        This part is under construction. Come back later.
+                    </h1>
+                    <img
+                        src="https://cdn-icons-png.flaticon.com/128/3967/3967195.png"
+                        alt="Under Construction Icon"
+                        className="w-24 h-24 md:w-32 md:h-32"
+                    />
                 </div>
             </div>
         </>
