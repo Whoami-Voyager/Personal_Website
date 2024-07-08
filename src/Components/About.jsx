434d8729178d5adce02170569f260c2536acfc51
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-07-08 14:46:59 -0600

    semantic html

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom"
-import ProjectCard from "./ProjectCard"
+import { Link } from "react-router-dom";
+import ProjectCard from "./ProjectCard";
 import { SiAdobelightroom, SiFlask, SiCloudinary, SiLeaflet } from "react-icons/si";
 import { IoLogoJavascript, IoLogoGithub } from "react-icons/io5";
 import { FaReact, FaPython, FaHtml5, FaCss3Alt, FaCamera, FaLinkedin } from "react-icons/fa";
@@ -7,8 +7,8 @@ import { RiTailwindCssFill } from "react-icons/ri";
 
 function About() {
     return (
-        <div className="bg-appleBeige h-full">
-            <div className="flex flex-row font-ThinkDifferent">
+        <div className="bg-appleBeige min-h-screen flex flex-col">
+            <header className="flex flex-row font-ThinkDifferent">
                 <h1 className="m-12 select-none text-center text-4xl lg:text-5xl w-1/5">Jonathan McIntosh</h1>
                 <div className="flex flex-col -skew-x-12 w-full m-6 lg:mr-12">
                     <div className="block m-0.5 bg-[#FBC02D] h-5 w-full"></div>
@@ -18,55 +18,63 @@ function About() {
                     <div className="block m-0.5 bg-[#7B1FA2] h-5 w-full"></div>
                     <div className="block m-0.5 bg-[#1976D2] h-5 w-full"></div>
                 </div>
-            </div>
+            </header>
             <nav className="font-ThinkDifferent m-4 flex flex-col items-center sm:flex-row sm:justify-center lg:justify-around lg:m-8">
                 <Link className="text-lg p-2 w-36 hover:text-terminalGreen hover:border-black hover:bg-black hover:scale-105 transition transform duration-100 border-4 border-appleAccent rounded-md text-center m-2" to="/coding">Coding Projects</Link>
                 <Link className="text-lg p-2 w-36 hover:text-terminalGreen hover:border-black hover:bg-black hover:scale-105 transition transform duration-100 border-4 border-appleAccent rounded-md text-center m-2" to="/photography">Photography</Link>
             </nav>
-            <div className="font-ThinkDifferent bg-appleBeige">
-                <h2 className="font-Cooper text-center p-8 text-3xl md:text-4xl lg:text-5xl bg-appleBeige select-none">Projects:</h2>
-                <ProjectCard />
-            </div>
-            <div className="font-ThinkDifferent bg-appleBeige p-8">
-                <h2 className="font-Cooper text-center p-8 pt-12 text-3xl md:text-4xl lg:text-5xl bg-appleBeige select-none">Skills:</h2>
-                <div className="flex flex-wrap justify-center gap-6 p-4 text-4xl">
-                    <IoLogoJavascript />
-                    <FaReact />
-                    <FaPython />
-                    <SiFlask />
-                    <RiTailwindCssFill />
-                    <FaHtml5 />
-                    <FaCss3Alt />
-                    <SiCloudinary />
-                    <SiLeaflet />
-                    <SiAdobelightroom />
-                    <FaCamera />
-                </div>
-            </div>
-            <h2 className="font-Cooper text-center p-8 pt-12 text-3xl md:text-4xl lg:text-5xl bg-appleBeige select-none">About myself:</h2>
-            <div className="flex flex-col text-center items-center gap-4 lg:gap-12 bg-appleBeige">
-                <p className="p-8 text-xl font-ttNorms">
-                    Hello! I'm Jonathan McIntosh, a full stack web developer with a background in photography and videography.<br />
-                    During my 15 weeks at Flatiron School, I mastered JavaScript, React, Python, Flask, and taught myself Tailwind CSS. I am a quick learner, always ready to acquire new skills.<br />
-                    To view my photography and videography portfolio, click on "Photography" at the top of the page to see my work.<br />
-                </p>
-                <div className="flex flex-row gap-5 lg:gap-10 items-center px-4">
-                    <a href="/Resume.pdf" download="Resume.pdf">
-                        <button className="bg-appleAccent lg:hover:scale-105 lg:hover:-translate-y-1 text-white font-Cooper text-xl p-3 rounded transition transform duration-300">Download Resume</button>
-                    </a>
-                    <a href="https://www.linkedin.com/in/jonathan-thomas-mcintosh/" target="_blank" className="lg:hover:scale-105 lg:hover:-translate-y-1 transition transform duration-300 text-5xl">
-                        <FaLinkedin />
-                    </a>
-                    <a href="https://github.com/Whoami-Voyager" target="_blank" className="lg:hover:scale-105 lg:hover:-translate-y-1 transition transform duration-300 text-5xl">
-                        <IoLogoGithub />
-                    </a>
-                </div>
-            </div>
-            <div className="flex justify-end text-sm pt-32 bg-appleBeige">
+
+            <main className="flex-grow">
+                <section className="font-ThinkDifferent bg-appleBeige">
+                    <h2 className="font-Cooper text-center p-8 text-3xl md:text-4xl lg:text-5xl bg-appleBeige select-none">Projects:</h2>
+                    <ProjectCard />
+                </section>
+
+                <section className="font-ThinkDifferent bg-appleBeige p-8">
+                    <h2 className="font-Cooper text-center p-8 pt-12 text-3xl md:text-4xl lg:text-5xl bg-appleBeige select-none">Skills:</h2>
+                    <div className="flex flex-wrap justify-center gap-6 p-4 text-4xl">
+                        <IoLogoJavascript />
+                        <FaReact />
+                        <FaPython />
+                        <SiFlask />
+                        <RiTailwindCssFill />
+                        <FaHtml5 />
+                        <FaCss3Alt />
+                        <SiCloudinary />
+                        <SiLeaflet />
+                        <SiAdobelightroom />
+                        <FaCamera />
+                    </div>
+                </section>
+
+                <section>
+                    <h2 className="font-Cooper text-center p-8 pt-12 text-3xl md:text-4xl lg:text-5xl bg-appleBeige select-none">About myself:</h2>
+                    <div className="flex flex-col text-center items-center gap-4 lg:gap-12 bg-appleBeige">
+                        <p className="p-8 text-xl font-ttNorms">
+                            Hello! I'm Jonathan McIntosh, a full stack web developer with a background in photography and videography.<br />
+                            During my 15 weeks at Flatiron School, I mastered JavaScript, React, Python, Flask, and taught myself Tailwind CSS. I am a quick learner, always ready to acquire new skills.<br />
+                            To view my photography and videography portfolio, click on "Photography" at the top of the page to see my work.<br />
+                        </p>
+                        <div className="flex flex-row gap-5 lg:gap-10 items-center px-4">
+                            <a href="/Resume.pdf" download="Resume.pdf">
+                                <button className="bg-appleAccent lg:hover:scale-105 lg:hover:-translate-y-1 text-white font-Cooper text-xl p-3 rounded transition transform duration-300">Download Resume</button>
+                            </a>
+                            <a href="https://www.linkedin.com/in/jonathan-thomas-mcintosh/" target="_blank" className="lg:hover:scale-105 lg:hover:-translate-y-1 transition transform duration-300 text-5xl">
+                                <FaLinkedin />
+                            </a>
+                            <a href="https://github.com/Whoami-Voyager" target="_blank" className="lg:hover:scale-105 lg:hover:-translate-y-1 transition transform duration-300 text-5xl">
+                                <IoLogoGithub />
+                            </a>
+                        </div>
+                    </div>
+                </section>
+            </main>
+
+            <footer className="flex justify-end text-sm pt-32 bg-appleBeige">
                 <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">🐇🐥🥚</a>
-            </div>
+            </footer>
         </div>
     )
 }
 
-export default About
+export default About;
--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -5,9 +5,11 @@ import { RiTailwindCssFill } from "react-icons/ri";
 
 function ProjectCard() {
     return (
-        <div className="flex flex-wrap justify-center gap-6 lg:gap-10">
-            <div className="card">
-                <h1 className="text-2xl 2xl:text-3xl mb-2 font-Cooper">LandmarkLore</h1>
+        <section className="flex flex-wrap justify-center gap-6 lg:gap-10">
+            <article className="card">
+                <header>
+                    <h1 className="text-2xl 2xl:text-3xl mb-2 font-Cooper">LandmarkLore</h1>
+                </header>
                 <div className="flex flex-row gap-4 text-3xl my-4">
                     <FaReact />
                     <RiTailwindCssFill />
@@ -16,73 +18,89 @@ function ProjectCard() {
                     <SiLeaflet />
                     <SiCloudinary />
                 </div>
-                <h2 className="text-xl mb-2">May 2024</h2>
-                <a href="https://github.com/Whoami-Voyager/LandmarkLore" target="_blank" className="text-appleAccent lg:hover:underline">GitHub Link</a>
-                <p className="text-base mt-2">My Phase 5 or capstone project for Flatiron School, I was able to build a website to tell stories using Leaflet.js. I also built it with image uploading using Cloudinary.</p>
-            </div>
-            <div className="card">
-                <h1 className="text-2xl 2xl:text-3xl mb-2 font-Cooper">InstaVibe</h1>
+                <div className="flex flex-col">
+                    <time className="text-xl mb-2">May 2024</time>
+                    <a href="https://github.com/Whoami-Voyager/LandmarkLore" target="_blank" className="text-appleAccent lg:hover:underline">GitHub Link</a>
+                </div>
+                <p className="text-base mt-2">For my Phase 5 capstone project at Flatiron School, I built LandmarkLore, a website that uses Leaflet.js for storytelling and Cloudinary for image uploading.</p>
+            </article>
+            <article className="card">
+                <header>
+                    <h1 className="text-2xl 2xl:text-3xl mb-2 font-Cooper">InstaVibe</h1>
+                </header>
                 <div className="flex flex-row gap-4 text-3xl my-4">
                     <FaReact />
                     <RiTailwindCssFill />
                     <FaPython />
                     <SiFlask />
                 </div>
-                <h2 className="text-xl mb-2">May 2024</h2>
-                <a href="https://github.com/Whoami-Voyager/InstaVibe" target="_blank" className="text-appleAccent lg:hover:underline">GitHub Link</a>
-                <p className="text-base mt-2">My Phase 4 project, I collaborated with a classmate for my first full-stack project. Using a React frontend and a Flask backend, I was able to build an Instagram replica.</p>
-            </div>
-            <div className="card">
-                <h1 className="text-2xl 2xl:text-3xl mb-2 font-Cooper">CarChronicle</h1>
+                <div className="flex flex-col">
+                    <time className="text-xl mb-2">May 2024</time>
+                    <a href="https://github.com/Whoami-Voyager/InstaVibe" target="_blank" className="text-appleAccent lg:hover:underline">GitHub Link</a>
+                </div>
+                <p className="text-base mt-2">InstaVibe, my Phase 4 project, is an Instagram replica built with a React frontend and Flask backend. This was my first full-stack project, developed in collaboration with a classmate.</p>
+            </article>
+            <article className="card">
+                <header>
+                    <h1 className="text-2xl 2xl:text-3xl mb-2 font-Cooper">CarChronicle</h1>
+                </header>
                 <div className="flex flex-row gap-4 text-3xl my-4">
                     <FaPython />
                 </div>
-                <h2 className="text-xl mb-2">April 2024</h2>
-                <a href="https://github.com/Whoami-Voyager/CarChronicle" target="_blank" className="text-appleAccent lg:hover:underline">GitHub Link</a>
-                <p className="text-base mt-2">My Phase 3 project, this was my project that introduced me to Python. A CLI, I used a web scraper beautiful soup to fill the SQL tables, and used Python classes to navigate the ORM.</p>
-            </div>
-            <div className="card">
-                <h1 className="text-2xl 2xl:text-3xl mb-2 font-Cooper">TechHistoria</h1>
+                <div className="flex flex-col">
+                    <time className="text-xl mb-2">April 2024</time>
+                    <a href="https://github.com/Whoami-Voyager/CarChronicle" target="_blank" className="text-appleAccent lg:hover:underline">GitHub Link</a>
+                </div>
+                <p className="text-base mt-2">CarChronicle, my Phase 3 project, introduced me to Python. This CLI app uses Beautiful Soup for web scraping and populating SQL tables, with Python classes for ORM navigation.</p>
+            </article>
+            <article className="card">
+                <header>
+                    <h1 className="text-2xl 2xl:text-3xl mb-2 font-Cooper">TechHistoria</h1>
+                </header>
                 <div className="flex flex-row gap-4 text-3xl my-4">
                     <FaReact />
                     <RiTailwindCssFill />
                 </div>
-                <h2 className="text-xl mb-2">March 2024</h2>
+                <time className="text-xl mb-2">March 2024</time>
                 <div className="flex flex-col gap-3">
                     <a href="https://whoami-voyager.github.io/TechHistoria/" target="_blank" className="text-appleAccent lg:hover:underline">Deployed Website</a>
                     <a href="https://github.com/Whoami-Voyager/TechHistoria" target="_blank" className="text-appleAccent lg:hover:underline">GitHub Link</a>
                 </div>
                 <p className="text-base mt-2">My Phase 2 project, This was my first website built using React. It also allowed me to start using Tailwind CSS, which made it easier to build my first website with responsive design.</p>
-            </div>
-            <div className="card">
-                <h1 className="text-2xl 2xl:text-3xl mb-2 font-Cooper">LeMans 100</h1>
+            </article>
+            <article className="card">
+                <header>
+                    <h1 className="text-2xl 2xl:text-3xl mb-2 font-Cooper">LeMans 100</h1>
+                </header>
                 <div className="flex flex-row gap-4 text-3xl my-4">
                     <IoLogoJavascript />
                     <FaHtml5 />
                     <FaCss3Alt />
                 </div>
-                <h2 className="text-xl mb-2">February 2024</h2>
+                <time className="text-xl mb-2">February 2024</time>
                 <div className="flex flex-col gap-3">
                     <a href="https://www.thelemans24.com/" target="_blank" className="text-appleAccent lg:hover:underline">Deployed Website</a>
                     <a href="https://github.com/thekaleabsamuel/Le-mans-100-v1" target="_blank" className="text-appleAccent lg:hover:underline">GitHub Link</a>
                 </div>
                 <p className="text-base mt-2">My Phase 1 project, I collaborated with a classmate to create a vanilla Javascript website. I used this project to expand my knowledge of flexbox in CSS, and using custom fonts with styling all around the page.</p>
-            </div>
-            <div className="card">
-                <h1 className="text-2xl 2xl:text-3xl mb-2 font-Cooper">My Favorite Things</h1>
+            </article>
+            <article className="card">
+                <header>
+                    <h1 className="text-2xl 2xl:text-3xl mb-2 font-Cooper">My Favorite Things</h1>
+                </header>
                 <div className="flex flex-row gap-4 text-3xl my-4">
                     <IoLogoJavascript />
                     <FaHtml5 />
                     <FaCss3Alt />
                 </div>
-                <h2 className="text-xl mb-2">November 2023</h2>
+                <time className="text-xl mb-2">November 2023</time>
                 <div className="flex flex-col gap-3">
                     <a href="https://whoami-voyager.github.io/My-Favorite-Things/" target="_blank" className="text-appleAccent lg:hover:underline">Deployed Website</a>
                     <a href="https://github.com/Whoami-Voyager/My-Favorite-Things" target="_blank" className="text-appleAccent lg:hover:underline">GitHub Link</a>
                 </div>
                 <p className="text-base mt-2">For my prework at Flatiron School, I had a final project to create my first website using HTML, CSS and a little Javascript. I was not required to do Javascript, but it was fun doing a keyframe animation.</p>
-            </div>
-        </div>
+            </article>
+        </section>
     );
 }
 
